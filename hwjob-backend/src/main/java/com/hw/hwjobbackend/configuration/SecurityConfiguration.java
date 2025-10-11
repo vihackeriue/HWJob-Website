1145bbde321265e57d70ad4928995e9e01cf15fa
Trịnh Nguyễn Nhật Hiếu <trinhnguyennhathieu@gmail.com>
2025-10-11 19:51:42 +0700

    save before merge

--- hwjob-backend/src/main/java/com/hw/hwjobbackend/configuration/SecurityConfiguration.java
+++ hwjob-backend/src/main/java/com/hw/hwjobbackend/configuration/SecurityConfiguration.java
@@ -1,11 +1,8 @@
 package com.hw.hwjobbackend.configuration;
 
 
-import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
