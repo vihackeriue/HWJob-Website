4afbef555751faa7d4c4fdbc1ba4eaf90be7111b
Trịnh Nguyễn Nhật Hiếu <trinhnguyennhathieu@gmail.com>
2025-11-07 23:41:43 +0700

    Job Type Pagination

--- hwjob-backend/src/main/java/com/hw/hwjobbackend/service/job_type/inplement/JobTypeServiceImpl.java
+++ hwjob-backend/src/main/java/com/hw/hwjobbackend/service/job_type/inplement/JobTypeServiceImpl.java
@@ -12,11 +12,11 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-
 
 @Service
 @RequiredArgsConstructor
@@ -50,9 +50,9 @@ public class JobTypeServiceImpl implements JobTypeService {
     }
 
     @Override
-    public List<JobTypeResponse> getAllJobTypes() {
-        return jobTypeRepository.findAll().stream().map(
-                jobTypeMapper::toJobTypeResponse).toList();
+    public Page<JobTypeResponse> getAllJobTypes(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return jobTypeRepository.findAll(pageable).map(jobTypeMapper::toJobTypeResponse);
     }
 
     @Override
