7aa26723ec919ca653749b29d96ef5678dffe1c3
Trịnh Nguyễn Nhật Hiếu <trinhnguyennhathieu@gmail.com>
2025-11-13 17:39:16 +0700

    Industry service

--- hwjob-backend/src/main/java/com/hw/hwjobbackend/dto/request/industry/IndustryRequest.java
+++ hwjob-backend/src/main/java/com/hw/hwjobbackend/dto/request/industry/IndustryRequest.java
@@ -1,18 +1,17 @@
-package com.hw.hwjobbackend.dto.response.industry;
+package com.hw.hwjobbackend.dto.request.industry;
 
+
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
-import java.util.List;
-
 @Getter
 @Setter
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class IndustryResponse {
-    Long id;
+public class IndustryRequest {
+    @NotBlank(message = " INDUSTRY_NOT_BLANK")
     String name;
     String description;
 }
--- hwjob-backend/src/main/java/com/hw/hwjobbackend/service/industry/IndustryService.java
+++ hwjob-backend/src/main/java/com/hw/hwjobbackend/service/industry/IndustryService.java
@@ -1,4 +1,18 @@
 package com.hw.hwjobbackend.service.industry;
 
+import com.hw.hwjobbackend.dto.request.industry.IndustryRequest;
+import com.hw.hwjobbackend.dto.response.industry.IndustryResponse;
+import org.springframework.data.domain.Page;
+
 public interface IndustryService {
+
+    Page<IndustryResponse> getAllIndustryNames(int page, int size);
+
+    IndustryResponse getIndustryById(long id);
+
+    IndustryResponse createIndustry(IndustryRequest request);
+
+    IndustryResponse updateIndustry(long id, IndustryRequest request);
+
+    void deleteIndustry(long id);
 }
