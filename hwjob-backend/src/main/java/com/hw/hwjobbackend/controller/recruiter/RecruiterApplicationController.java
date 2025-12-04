892e9698126730609ae289649a3c59b9f7b86e2f
Trịnh Nguyễn Nhật Hiếu <trinhnguyennhathieu@gmail.com>
2025-12-04 17:48:26 +0700

    Fix endpoint

--- hwjob-backend/src/main/java/com/hw/hwjobbackend/controller/recruiter/RecruiterApplicationController.java
+++ hwjob-backend/src/main/java/com/hw/hwjobbackend/controller/recruiter/RecruiterApplicationController.java
@@ -21,7 +21,7 @@ public class RecruiterApplicationController {
 
     RecruiterApplicationService recruiterJobPostService;
 
-    @GetMapping("/{jobPostId}/candidates")
+    @GetMapping("/job-posts/{jobPostId}/candidates")
     public ApiResponse<List<ApplicationCandidateResponse>> getCandidatesAppliedJob(
             @PathVariable String jobPostId,
             @RequestParam(value = "page", required = false) Integer page,
@@ -45,7 +45,7 @@ public class RecruiterApplicationController {
                 .build();
     }
 
-    @PatchMapping("/{jobPostId}/candidates/{candidateId}/status")
+    @PatchMapping("/job-posts/{jobPostId}/candidates/{candidateId}/status")
     public ApiResponse<Void> updateCandidateApplicationStatus(
             @PathVariable String jobPostId,
             @PathVariable String candidateId,
