a05bb88791214d25cca954df51c0063075b59258
Trịnh Nguyễn Nhật Hiếu <trinhnguyennhathieu@gmail.com>
2025-12-28 09:54:15 +0700

    Save before checkout to dev

--- hwjob-backend/src/main/java/com/hw/hwjobbackend/repository/user/UserRepository.java
+++ hwjob-backend/src/main/java/com/hw/hwjobbackend/repository/user/UserRepository.java
@@ -3,14 +3,11 @@ package com.hw.hwjobbackend.repository.user;
 import com.hw.hwjobbackend.model.entity.user.Recruiter;
 import com.hw.hwjobbackend.model.entity.user.Role;
 import com.hw.hwjobbackend.model.entity.user.User;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
